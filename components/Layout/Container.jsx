6fb86c86f8f9845ae49a77a0579304d5e882045b
Ollie Beenham <ollie@beenhamow.co.uk>
2024-04-29 19:41:04 +0100

    Reset Repository

--- components/Layout/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children }) {
-    return (
-        <div className="w-full h-full flex justify-center px-8 border-solid">
-            <div className="w-full max-w-6xl">{children}</div>
-        </div>
-    );
-}
