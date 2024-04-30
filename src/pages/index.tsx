10d0ad34957a901bfb5117341f594a5504e1bf8e
Ollie Beenham <ollie@beenhamow.co.uk>
2024-04-30 11:09:52 +0100

    Added responsive navbar and a 404 page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Layout from "@/components/layout";
+import Layout from "@/components/Layout";
 
 export default function Home() {
     return (
         <Layout title="Home">
-            <div className="w-full h-[100dvh] flex items-center justify-center p-10">
+            <div className="w-full h-full flex items-center justify-center p-10 flex-grow">
                 <p className="text-center">
                     This page is currently under construction. To view the university website,{" "}
                     <a className="underline" href="https://www.ljmu.ac.uk">
